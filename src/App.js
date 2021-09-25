a9759fdc7a8396d64b8b6e3925b7b4d018bb630a
CodeCostylProduction <starostin039@gmail.com>
2021-09-25 16:13:19 +0300

    up to 44 exercise

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Layout from "./hoc/Layout/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello react</h1>
-    </div>
+    <Layout>
+        <div style={{
+            margin: '0 auto',
+            border: '1pt solid black',
+            width: 300,
+            textAlign: 'center'
+        }}>
+            <h1>Layout works</h1>
+        </div>
+    </Layout>
   );
 }
 
