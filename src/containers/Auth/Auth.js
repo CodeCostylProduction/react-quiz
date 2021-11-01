578eca7ed384dd71e83fb21276453551556eddb4
CodeCostylProduction <starostin039@gmail.com>
2021-11-01 13:21:02 +0300

    up to 76 exercise

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import classes from './Auth.css'
 import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/input/input";
 
 export default class Auth extends Component {
     loginHandler = () => {
@@ -18,9 +19,9 @@ export default class Auth extends Component {
                 <div>
                     <h1>Авторизация</h1>
                     <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-                        <input type="text"/>
-                        <input type="text"/>
-
+                        <Input label="Email"/>
+                        <Input label="Пароль"
+                        />
                         <Button type="success" onClick={this.loginHandler}>
                             Войти
                         </Button>
