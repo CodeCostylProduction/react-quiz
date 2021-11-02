1d5614fcb51af6e15c2f54a552082237951c1598
CodeCostylProduction <starostin039@gmail.com>
2021-11-02 13:38:19 +0300

    up to 79 exercise

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,6 +7,7 @@ import is from 'is_js'
 export default class Auth extends Component {
 
     state = {
+        isFormValid: false,
         formControls: {
             email: {
                 value: '',
@@ -75,8 +76,14 @@ export default class Auth extends Component {
 
         formControls[controlName] = control
 
+        let isFormValid = true
+
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid
+    })
+
         this.setState({
-            formControls
+            formControls, isFormValid
         })
     }
 
@@ -106,10 +113,17 @@ export default class Auth extends Component {
                     <h1>Авторизация</h1>
                     <form onSubmit={this.submitHandler} className={classes.AuthForm}>
                         {this.renderInputs()}
-                        <Button type="success" onClick={this.loginHandler}>
+                        <Button
+                            type="success"
+                            onClick={this.loginHandler}
+                            disabled={!this.state.isFormValid}
+                        >
                             Войти
                         </Button>
-                        <Button type="primary" onClick={this.registerHandler}>
+                        <Button type="primary"
+                                onClick={this.registerHandler}
+                                disabled={!this.state.isFormValid}
+                        >
                             Зарегистрироваться
                         </Button>
                     </form>
