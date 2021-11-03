cb52b3609e155a773ba9b6d38570e0303fba83c2
CodeCostylProduction <starostin039@gmail.com>
2021-11-03 15:45:16 +0300

    up to 84 exercise

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import classes from './Quiz.css'
 import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 import FinishedQuiz from "../../components/FinishedQuiz/FinishedQuiz";
