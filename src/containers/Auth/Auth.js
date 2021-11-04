3d8996b3c932aa932ae70664ab0e52438c29e31e
CodeCostylProduction <starostin039@gmail.com>
2021-11-04 14:51:58 +0300

    up to 90 exercise

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.css'
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/input/input";
 import is from 'is_js'
+import axios from "axios";
 
 export default class Auth extends Component {
 
@@ -36,11 +37,33 @@ export default class Auth extends Component {
         }
     }
 
-    loginHandler = () => {
-
+    loginHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        }
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBhx9uYSB9zqZM53noYjNTNba2lYGpWnJQ', authData)
+            console.log(response.data)
+        }
+        catch (e) {
+            console.log(e)
+        }
     }
-    registerHandler = () => {
-
+    registerHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        }
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBhx9uYSB9zqZM53noYjNTNba2lYGpWnJQ', authData)
+            console.log(response.data)
+        }
+        catch (e) {
+           console.log(e)
+        }
     }
     submitHandler = event => {
         event.preventDefault()
