bc69e930d6fb9ae9cf3e199105e0590afb96a78b
CodeCostylProduction <starostin039@gmail.com>
2021-11-06 13:35:24 +0300

    up to 102 exercise from 101

--- src/index.js
+++ src/index.js
@@ -4,11 +4,29 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from "./store/reducers/rootReducer";
+import {compose, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+
 )
 
 ReactDOM.render(
